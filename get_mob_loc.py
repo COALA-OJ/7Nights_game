e41e61571ff2fb2fcb2d4c953bd500f6aa8121e1
jeslsy <jeslsy0507@naver.com>
2021-01-02 01:12:29 +0900

    get_mob_loc.py

--- get_mob_loc.py
+++ get_mob_loc.py
@@ -1,19 +1,20 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+def get_mob_loc(map):
+    n = len(List)
+    m = len(List[0])
+
+    newlist = []
+    for i in range(n):
+        for j in range(m):
+            if List[i][j] == 2:
+                newlist.append([i, j])
+
+    print(newlist)
+
+
 List = [[1,0,0,2],[3,3,2,1],[2,2,0,1]]
-
-n = len(List)
-m = len(List[0])
-
-newlist = []
-for i in range(n):
-    for j in range(m):
-        if List[i][j] == 2:
-            newlist.append([i, j])
-
-print(newlist)
-
-
+get_mob_loc(List)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/

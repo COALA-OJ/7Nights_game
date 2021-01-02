d024f80d5f83cb509b2915ab531e384b1d733775
tjdans6342 <01tjdans@naver.com>
2021-01-02 23:08:02 +0900

    get_mosterid.py

--- get_mosterid.py
+++ get_mosterid.py
@@ -4,4 +4,4 @@ def get_mosterid(x,y):
     for key,value in monster.items():
         # for문을 돌다가 mob_info중 x좌표와 y좌표가 입력의 x,y와 같을 때 return
         if(value[0][0] == x and value[0][1] == y):
-            return [[[value[0][0],value[0][1]],value[1],value[2]],user_info]
+            return [[[value[0][0],value[0][1]],getmonster_hp(key),getmonster_dmg(key)],user_info]

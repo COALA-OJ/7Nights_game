2cadce947aa169457ee246c5cb566633144b2f2f
wittch <hwan95212@naver.com>
2021-01-02 19:24:14 +0900

    create init_map.py

---
+++ init_map.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+class init_map:
+    def __init__(self, n, m):
+        #n = int(input('입력 n : '))
+        #m = int(input('입력 m : '))
+        mapnumpy = np.zeros((n,m))
+        
+        return mapnumpy

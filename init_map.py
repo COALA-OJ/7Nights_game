b0019d30d73d19ee5d642742a9e6b0a65a1d2b66
wittch <hwan95212@naver.com>
2021-01-03 00:59:13 +0900

    fixed init_map.py's tab

--- init_map.py
+++ init_map.py
@@ -1,11 +1,10 @@
 def init_map(n, m):
      
-        maplist = []
-        for i in range(n):
-            line = []
-            for j in range(m):
-                line.append(0)
-            maplist.append(line)
+    maplist = []
+    for i in range(n):
+        line = []
+        for j in range(m):
+            line.append(0)
+        maplist.append(line)
             
-        
-        return maplist
+    return maplist

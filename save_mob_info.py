88afa64f75ee39b6b02c7425a931f37a6e824dec
hodu26 <69331491+hodu26@users.noreply.github.com>
2021-01-02 23:30:59 +0900

    Delete save_mob_info.py

--- save_mob_info.py
+++
@@ -1,9 +0,0 @@
-def save_mob_info(mob_loc, user_create_info):
-
-    for id in range(len(mob_loc)):
-        mobINFO = random_making_mob()  # mobHP, mobDMG 불러오기
-        mob.update({id: [mob_loc[id],mobINFO[0],mobINFO[1]]})  # mob추가 -> key : 0 ~ len-1
-
-    user_info = user_create_info
-
-    return True

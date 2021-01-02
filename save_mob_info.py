79e4beee7c6e80c88cb92f903adf03ea623c6a28
hodu <gjdhks1212@gmail.com>
2021-01-02 23:07:06 +0900

    fix_1 save_mob_info.py

--- save_mob_info.py
+++ save_mob_info.py
@@ -1,7 +1,9 @@
-def save_mob_info(mob_loc):
+def save_mob_info(mob_loc, user_create_info):
 
     for id in range(len(mob_loc)):
         mobINFO = random_making_mob()  # mobHP, mobDMG 불러오기
         mob.update({id: [mob_loc[id],mobINFO[0],mobINFO[1]]})  # mob추가 -> key : 0 ~ len-1
 
-    return
+    user_info = user_create_info
+
+    return True

dab8e15770e16ad9a0568a3350da6f49fb47a421
DESKTOP-SSTRNI0\HOON <lee040118@naver.com>
2021-01-06 16:59:32 +0900

    Update_event_code_0106_hoon

--- main.py
+++ main.py
@@ -1,26 +1,39 @@
 import requests, json, sys
-from settings import *
-
-'''
-GET 방식으로 보내기
-'''
+import requests
+import json
+from react import *
+#global usermap
 def initiate_game():
-    res = requests.get("http://127.0.0.1:5000/spring/initiate_game%22").text
-    if res=="True": return True
-    else: return False
+    res = json.loads(requests.get("http://211.33.49.253:5000/spring/initiate_game").text)
+    #print(res['user_create_info'])
+    """
+    초기 맵 상태, 초기 유저 위치 확인인
+    # """
+    with open('map_file3', 'w', encoding='utf-8') as file:
+        for i in res['map']:
+            file.write(" ".join([str(_) for _ in i]))
+            file.write('\n')
 
-'''
-POST 방식으로 보내기
-'''
-# headers = {'Content-Type': 'application/json'}
-# data = {
-#     'id': 'lee',
-#     'lang_type': 'cpp',
-# }
-# res = requests.post('http://210.117.181.248:13577/3', headers=headers, data=json.dumps(data)).text
+    with open('user_pos', 'w', encoding='utf-8') as file:
+        for i in res['user_create_info']:
+            file.write(str(i)+"\n")
+    # usermap = ''
+    # for i in res['map']:
+    #     usermap+=" ".join([str(_) for _ in i])
+    #     usermap+='\n'
 
-if __name__ == 'main':
-    can_start = initiate_game()
-    if can_start==False:
-        sys.exit()
-    # 계속 진행
+# @app.route('/react/meet_monster')
+def send_event(pos_x,pos_y):
+    data = {'user_pos_x':'', 'user_pos_y' : ''}
+    data['user_pos_x'] =  pos_x
+    data['user_pos_y'] =  pos_y
+    res = requests.get('http://211.33.49.253:5000/spring/meet_monster', params=data, timeout=2)
+    #res = requests.get('http://127.0.0.1:5000/react/meet_monster', params=data, timeout=2)
+
+    return res
+
+if __name__=='__main__':
+    initiate_game()
+    g = UserState()
+    while g.start:
+        g.new()

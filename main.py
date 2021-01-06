21775e349e480d207685dfe5d148a584dde4d302
DESKTOP-SSTRNI0\HOON <lee040118@naver.com>
2021-01-06 22:00:27 +0900

    Update_get_Item 0106_Hoon

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ import time
 #global usermap
 def initiate_game():
     res = json.loads(requests.get("http://211.33.49.253:5000/spring/initiate_game").text)
-    #print(res['user_create_info'])
     """
     초기 맵 상태, 초기 유저 위치 확인인
     # """
@@ -18,21 +17,17 @@ def initiate_game():
     with open('user_pos', 'w', encoding='utf-8') as file:
         for i in res['user_create_info']:
             file.write(str(i)+"\n")
-    # usermap = ''
-    # for i in res['map']:
-    #     usermap+=" ".join([str(_) for _ in i])
-    #     usermap+='\n'
 
-# @app.route('/react/meet_monster')
 def send_event(event, pos_x,pos_y):
     data = {'w_event':'','user_pos_x':'', 'user_pos_y' : ''}
     data['w_event'] = event
     data['user_pos_x'] =  pos_x
     data['user_pos_y'] =  pos_y
     start = time.time()
-    res = requests.get('http://211.33.49.253:5000/spring/meet_monster', params=data, timeout=5)
+    header = {'Content-Type':'application/json'}
+    res = requests.post('http://211.33.49.253:5000/spring/meet_monster',headers=header, data=json.dumps(data), timeout=5).json()
     end = time.time()
-    print(f"time : {end-start}")
+    #print(res["item_loc"])
     return res
 
 if __name__=='__main__':

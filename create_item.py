cad2b4739726ce09132332cc6f0a80e1b7472eee
wittch <hwan95212@naver.com>
2021-01-06 15:20:28 +0900

    fixed error 'create_item.py' ver1.1

--- create_item.py
+++ create_item.py
@@ -3,17 +3,20 @@ import random
 
 def create_item(maplist, curx, cury):
     
-    
+    wallcount=0
     nummax = [0,0]
     map_max=max(maplist)
-    for i in map_max:
+    nummax[0]=len(maplist)
+    nummax[1]=len(map_max)
+    nummax[0]-=1
+    nummax[1]-=1
+    """for i in map_max:
         for j in range(1, 3):
-            nummax[j-1]=i
+            nummax[j-1]=i"""
     
     if curx==0 and cury==0: #curx cury가 모두 0일 때 right_top
         itemlist = []
         ranlist = [0,1]
-        rannum = random.randrange(1,4)
         
         visited = [[True, False],
                    [False, False]]
@@ -22,10 +25,12 @@ def create_item(maplist, curx, cury):
             for j in range(0,2):
                 if maplist[curx+i][cury+j]==1:
                     visited[i][j] = True
-                    rannum-=1
+                    wallcount-=1
+        wtemp = 4+wallcount
+        rannum = random.randrange(1,wtemp)
                     
-        tempitemlist = [[curx, cury],[curx+1, cury],
-                        [curx, cury+1],[curx+1, cury+1]]
+        tempitemlist = [[curx, cury],[curx, cury+1],
+                    [curx+1, cury],[curx+1, cury+1]]
         
     
         
@@ -47,17 +52,19 @@ def create_item(maplist, curx, cury):
         for i in range(0,2):
             for j in range(0,2):
                 itemlist.append([])
-                if visited[i][j]==True:
+                if visited[i][j]==True and maplist[curx+i][cury+j]==0:
                     itemlist[itemcount].append(tempitemlist[itemcount][0])
                     itemlist[itemcount].append(tempitemlist[itemcount][1])
                 itemcount+=1
         itemlist.sort(reverse=True)
         
+       
+        
     elif curx==0 and cury==nummax[1]: #curx, cury가 우측 상단일 때 left_top
     
         itemlist = []
         ranlist = [0,1]
-        rannum = random.randrange(1,4)
+        
         
         visited = [[False, True],
                    [False, False]]
@@ -66,11 +73,13 @@ def create_item(maplist, curx, cury):
             for j in range(-1,1):
                 if maplist[curx+i][cury+j]==1:
                     visited[i][j+1] = True
-                    rannum-=1
+                    wallcount-=1
+        wtemp = 4+wallcount
+        rannum = random.randrange(1,wtemp)
         
         
-        tempitemlist = [[curx-1, cury],[curx, cury],
-                        [curx-1, cury+1],[curx, cury+1]]
+        tempitemlist = [[curx, cury-1],[curx, cury],
+                    [curx+1, cury-1],[curx+1, cury]]
         
     
         
@@ -84,7 +93,7 @@ def create_item(maplist, curx, cury):
             visited[ranx][rany]=True
          
           
-        visited[0][0]=False
+        visited[0][1]=False
          
 
          
@@ -92,19 +101,21 @@ def create_item(maplist, curx, cury):
         itemcount=0
          
         for i in range(0,2):
-            for j in range(0,2):
+            for j in range(-1,1):
                 itemlist.append([])
-                if visited[i][j]==True:
+                if visited[i][j+1]==True and maplist[curx+i][cury+j]==0:
                     itemlist[itemcount].append(tempitemlist[itemcount][0])
                     itemlist[itemcount].append(tempitemlist[itemcount][1])
                 itemcount+=1
         itemlist.sort(reverse=True)
         
+   
+        
     elif curx==nummax[0] and cury==0: #curx, cury가 왼쪽 하단일 때 right_bottom
         
         itemlist = []
         ranlist = [0,1]
-        rannum = random.randrange(1,4)
+        
         
         visited = [[False, False],
                    [True, False]]
@@ -113,10 +124,12 @@ def create_item(maplist, curx, cury):
             for j in range(0,2):
                 if maplist[curx+i][cury+j]==1:
                     visited[i+1][j] = True
-                    rannum-=1
+                    wallcount-=1
+        wtemp = 4+wallcount
+        rannum = random.randrange(1,wtemp)
                     
-        tempitemlist = [[curx, cury-1],[curx+1, cury-1],
-                        [curx, cury],[curx+1, cury]]
+        tempitemlist = [[curx-1, cury],[curx-1, cury+1],
+                        [curx, cury],[curx, cury+1]]
     
         
         for i in range(1, rannum+1):
@@ -129,26 +142,28 @@ def create_item(maplist, curx, cury):
             visited[ranx][rany]=True
          
           
-        visited[0][0]=False
+        visited[1][0]=False
          
         
         itemlist = []
         itemcount=0
          
-        for i in range(0,2):
+        for i in range(-1,1):
             for j in range(0,2):
                 itemlist.append([])
-                if visited[i][j]==True:
+                if visited[i+1][j]==True and maplist[curx+i][cury+j]==0:
                     itemlist[itemcount].append(tempitemlist[itemcount][0])
                     itemlist[itemcount].append(tempitemlist[itemcount][1])
                 itemcount+=1
         itemlist.sort(reverse=True)
         
+    
+        
     elif curx==nummax[0] and cury==nummax[1]: #curx, cury가 오른쪽 하단일 때 left_bottom
     
         itemlist = []
         ranlist = [0,1]
-        rannum = random.randrange(1,4)
+        
         
         visited = [[False, False],
                    [False, True]]
@@ -157,11 +172,13 @@ def create_item(maplist, curx, cury):
             for j in range(-1,1):
                 if maplist[curx+i][cury+j]==1:
                     visited[i+1][j+1] = True
-                    rannum-=1  
+                    wallcount-=1
+        wtemp = 4+wallcount
+        rannum = random.randrange(1,wtemp)  
         
         
-        tempitemlist = [[curx-1, cury-1],[curx, cury-1],
-                        [curx-1, cury],[curx, cury]]
+        tempitemlist = [[curx-1, cury-1],[curx-1, cury],
+                        [curx, cury-1],[curx, cury]]
         
     
         
@@ -175,29 +192,31 @@ def create_item(maplist, curx, cury):
             visited[ranx][rany]=True
          
           
-        visited[0][0]=False
+        visited[1][1]=False
          
-        print(visited)
+        
          
        
         itemlist = []
         itemcount=0
          
-        print(tempitemlist)
-        for i in range(0,2):
-            for j in range(0,2):
+        
+        for i in range(-1,1):
+            for j in range(-1,1):
                 itemlist.append([])
-                if visited[i][j]==True:
+                if visited[i+1][j+1]==True and maplist[curx+i][cury+j]==0:
                     itemlist[itemcount].append(tempitemlist[itemcount][0])
                     itemlist[itemcount].append(tempitemlist[itemcount][1])
                 itemcount+=1
         itemlist.sort(reverse=True)
         
-    elif curx==0: #curx가 0일 때
+    
+        
+    elif cury==0: #cury가 0일 때
         itemlist = []
         ranlistx = [0,1,2]
         ranlisty = [0,1]
-        rannum = random.randrange(1,6)
+       
           
           
         visited = [[False, False],
@@ -209,11 +228,13 @@ def create_item(maplist, curx, cury):
             for j in range(0,2):
                 if maplist[curx+i][cury+j]==1:
                     visited[i+1][j] = True
-                    rannum-=1
+                    wallcount-=1
+        wtemp = 6+wallcount
+        rannum = random.randrange(1,wtemp)
             
-        tempitemlist = [[curx, cury-1],[curx+1, cury-1],
-                        [curx, cury],[curx+1, cury],
-                        [curx, cury+1],[curx+1, cury+1]]
+        tempitemlist = [[curx-1, cury],[curx-1, cury+1],
+                        [curx, cury],[curx, cury+1],
+                        [curx+1, cury],[curx+1, cury+1]]
         
         
         for i in range(1, rannum+1):
@@ -232,20 +253,20 @@ def create_item(maplist, curx, cury):
         itemlist = []
         itemcount=0
          
-        for i in range(0,3):
+        for i in range(-1,2):
             for j in range(0,2):
                 itemlist.append([])
-                if visited[i][j]==True:
+                if visited[i+1][j]==True and maplist[curx+i][cury+j]==0:
                     itemlist[itemcount].append(tempitemlist[itemcount][0])
                     itemlist[itemcount].append(tempitemlist[itemcount][1])
                 itemcount+=1
         itemlist.sort(reverse=True)
         
-    elif cury==0: #cury가 0일 때
+    elif curx==0: #curx가 0일 때
         itemlist = []
         ranlistx = [0,1]
         ranlisty = [0,1,2]
-        rannum = random.randrange(1,6)
+        
           
           
         visited = [[False, True, False],
@@ -255,11 +276,13 @@ def create_item(maplist, curx, cury):
             for j in range(-1,2):
                 if maplist[curx+i][cury+j]==1:
                     visited[i][j+1] = True
-                    rannum-=1
+                    wallcount-=1
+        wtemp = 6+wallcount
+        rannum = random.randrange(1,wtemp)
         
         
-        tempitemlist = [[curx-1, cury],[curx, cury],[curx+1, cury],
-                        [curx-1, cury+1],[curx, cury+1],[curx+1, cury+1]]
+        tempitemlist = [[curx, cury-1],[curx, cury],[curx, cury+1],
+                        [curx+1, cury-1],[curx+1, cury],[curx+1, cury+1]]
         
         
         for i in range(1, rannum+1):
@@ -279,57 +302,9 @@ def create_item(maplist, curx, cury):
         itemcount=0
          
         for i in range(0,2):
-            for j in range(0,3):
+            for j in range(-1,2):
                 itemlist.append([])
-                if visited[i][j]==True:
-                    itemlist[itemcount].append(tempitemlist[itemcount][0])
-                    itemlist[itemcount].append(tempitemlist[itemcount][1])
-                itemcount+=1
-        itemlist.sort(reverse=True)
-        
-    elif curx==nummax[0]: #curx가 mapx의 max값일 때
-
-        itemlist = []
-        ranlistx = [0,1,2]
-        ranlisty = [0,1]
-        rannum = random.randrange(1,6)
-          
-          
-        visited = [[False, False],
-                   [False, True],
-                   [False, False]]
-        
-        for i in range(-1,2):
-            for j in range(-1,1):
-                if maplist[curx+i][cury+j]==1:
-                    visited[i+1][j+1] = True
-                    rannum-=1
-        
-        tempitemlist = [[curx-1, cury-1],[curx, cury-1],
-                        [curx-1, cury],[curx, cury],
-                        [curx-1, cury+1],[curx, cury+1]]
-        
-        
-        for i in range(1, rannum+1):
-            ranx = random.choice(ranlistx)
-            rany = random.choice(ranlisty)
-            if visited[ranx][rany]==True:
-                while visited[ranx][rany]:
-                    ranx = random.choice(ranlistx)
-                    rany = random.choice(ranlisty)
-            visited[ranx][rany]=True
-         
-          
-        visited[1][0]=False
-         
-  
-        itemlist = []
-        itemcount=0
-         
-        for i in range(0,3):
-            for j in range(0,2):
-                itemlist.append([])
-                if visited[i][j]==True:
+                if visited[i][j+1]==True and maplist[curx+i][cury+j]==0:
                     itemlist[itemcount].append(tempitemlist[itemcount][0])
                     itemlist[itemcount].append(tempitemlist[itemcount][1])
                 itemcount+=1
@@ -340,20 +315,24 @@ def create_item(maplist, curx, cury):
         itemlist = []
         ranlistx = [0,1,2]
         ranlisty = [0,1]
-        rannum = random.randrange(1,6)
-          
-          
-        visited = [[False, False, False],
-                   [False, True, False]]
         
-        for i in range(-1,1):
-            for j in range(-1,2):
+          
+          
+        visited = [[False, False],
+                   [False, True],
+                   [False, False]]
+        
+        for i in range(-1,2):
+            for j in range(-1,1):
                 if maplist[curx+i][cury+j]==1:
                     visited[i+1][j+1] = True
-                    rannum-=1 
+                    wallcount-=1
+        wtemp = 6+wallcount
+        rannum = random.randrange(1,wtemp)
         
-        tempitemlist = [[curx-1, cury-1],[curx, cury-1],[curx+1, cury-1],
-                        [curx-1, cury],[curx, cury],[curx+1, cury]]
+        tempitemlist = [[curx-1, cury-1],[curx-1, cury],
+                        [curx, cury-1],[curx, cury],
+                        [curx+1, cury-1],[curx+1, cury],]
         
         
         for i in range(1, rannum+1):
@@ -366,16 +345,64 @@ def create_item(maplist, curx, cury):
             visited[ranx][rany]=True
          
           
-        visited[1][0]=False
+        visited[1][1
+                   ]=False
+         
+  
+        itemlist = []
+        itemcount=0
+         
+        for i in range(-1,2):
+            for j in range(-1,1):
+                itemlist.append([])
+                if visited[i+1][j+1]==True and maplist[curx+i][cury+j]==0:
+                    itemlist[itemcount].append(tempitemlist[itemcount][0])
+                    itemlist[itemcount].append(tempitemlist[itemcount][1])
+                itemcount+=1
+        itemlist.sort(reverse=True)
+        
+    elif curx==nummax[0]: #curx가 mapx의 max값일 때
+
+        itemlist = []
+        ranlistx = [0,1]
+        ranlisty = [0,1,2]
+        
+          
+          
+        visited = [[False, False, False],
+                   [False, True, False]]
+        
+        for i in range(-1,1):
+            for j in range(-1,2):
+                if maplist[curx+i][cury+j]==1:
+                    visited[i+1][j+1] = True
+                    wallcount-=1
+        wtemp = 6+wallcount
+        rannum = random.randrange(1,wtemp)
+        
+        tempitemlist = [[curx-1, cury-1],[curx-1, cury],[curx-1, cury+1],
+                        [curx, cury-1],[curx, cury],[curx, cury+1]]
+        
+        for i in range(1, rannum+1):
+            ranx = random.choice(ranlistx)
+            rany = random.choice(ranlisty)
+            if visited[ranx][rany]==True:
+                while visited[ranx][rany]:
+                    ranx = random.choice(ranlistx)
+                    rany = random.choice(ranlisty)
+            visited[ranx][rany]=True
+         
+          
+        visited[1][1]=False
          
 
         itemlist = []
         itemcount=0
          
-        for i in range(0,3):
-            for j in range(0,2):
+        for i in range(-1,1):
+            for j in range(-1,2):
                 itemlist.append([])
-                if visited[i][j]==True:
+                if visited[i+1][j+1]==True and maplist[curx+i][cury+j]==0:
                     itemlist[itemcount].append(tempitemlist[itemcount][0])
                     itemlist[itemcount].append(tempitemlist[itemcount][1])
                 itemcount+=1
@@ -395,12 +422,14 @@ def create_item(maplist, curx, cury):
             for j in range(-1,2):
                 if maplist[curx+i][cury+j]==1:
                     visited[i+1][j+1] = True
-                    rannum-=1
+                    wallcount-=1
+        wtemp = 9+wallcount
+        rannum = random.randrange(1,wtemp)
         
         
-        tempitemlist = [[curx-1, cury-1],[curx, cury-1],[curx+1, cury-1],
-                        [curx-1, cury],[curx, cury],[curx+1, cury],
-                        [curx-1, cury+1],[curx, cury+1],[curx+1, cury+1]]
+        tempitemlist = [[curx-1, cury-1],[curx-1, cury],[curx-1, cury+1],
+                    [curx, cury-1],[curx, cury],[curx, cury+1],
+                    [curx+1, cury-1],[curx+1, cury],[curx+1, cury+1]]
         
         
         for i in range(1, rannum+1):
@@ -419,14 +448,16 @@ def create_item(maplist, curx, cury):
         itemlist = []
         itemcount=0
          
-        for i in range(0,3):
-            for j in range(0,3):
+        for i in range(-1,2):
+            for j in range(-1,2):
                 itemlist.append([])
-                if visited[i][j]==True:
+                if visited[i+1][j+1]==True and maplist[curx+i][cury+j]==0:
                     itemlist[itemcount].append(tempitemlist[itemcount][0])
                     itemlist[itemcount].append(tempitemlist[itemcount][1])
                 itemcount+=1
-        itemlist.sort(reverse=True)
+    itemlist.sort(reverse=True)
+    
     
     
     return itemlist[:rannum]
+

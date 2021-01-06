28d02c0b78e8bd8f4c92cfc95fd5a57469a75113
soosungp33 <soosungp33@gmail.com>
2021-01-06 18:12:50 +0900

    Modify component size

--- make_wall.py
+++ make_wall.py
@@ -1,41 +1,45 @@
 from collections import deque
 import random
+import copy
 
 def make_wall(list):
-    dx = [0,0,1,-1]
-    dy = [1,-1,0,0]
-    mx = len(list[0])
-    my = len(list)
+    dx = [1, -1, 0, 0]
+    dy = [0, 0, 1, -1]
+    choice = [4,5]
+    mx = len(list)
+    my = len(list[0])
     # 컴포넌트 갯수 설정
     component = random.randint(10, 15)
     for i in range(component):
+        print(i)
         # 1의 넓이 설정
-        area = random.randint(10, 30)
+        area = random.randint(0, 1)
 
         # 1이 아닌 한 지점 선택
-        temp = list
-        x, y
+        temp = copy.deepcopy(list)
+        x=0
+        y=0
         while True:
-            x = random.randint(0,mx)
-            y = random.randint(0,my)
+            x = random.randint(0, mx-1)
+            y = random.randint(0, my-1)
             if temp[x][y] == 0:
                 break
 
-        # 그 지점을 기준으로 설정한 넓이만큼 1로 변경
+        # 그 지점을 기준으로 4x4 or 5x5 넓이만큼 1로 변경
         temp[x][y] = 1
-        for j in range(area):
-            go = random.randint(0,3)
-            nx = x+dx[go]
-            ny = y+dy[go]
-            if (temp[nx][ny]==1 or nx<0 or nx>=mx or ny<0 or ny>=my):
-                j-=1
-                continue
-            temp[nx][ny]=1
-            x = nx
-            y = ny
+        for j in range(choice[area]):
+            for k in range(choice[area]):
+
+                nx = x+j
+                ny = y+k
+                if (x < 0 or nx >= mx or ny < 0 or ny >= my):
+                    continue
+                temp[nx][ny]=1
+                x = nx
+                y = ny
 
         # 모든 지점을 탐색하기 위한 1이 아닌 지점 선택
-        sx, sy
+        sx=0; sy=0
         check1 = True;
         for searchx in range(mx):
             if check1 == False:
@@ -49,7 +53,7 @@ def make_wall(list):
         # 모든 지점 탐색
         queue = deque()
         queue.append((sx,sy))
-        search = temp
+        search = copy.deepcopy(temp)
         search[sx][sy] = 1
         while queue:
             qx, qy = queue.popleft()
@@ -58,10 +62,9 @@ def make_wall(list):
                 qny = qy + dy[j]
 
                 # 범위 밖 and 갈 수 없는 곳
-                if(search[qnx][qny] == 1 or qnx<0 or qnx>=mx or qny<0 or qny>=my) : continue
-
-                queue.append(qnx, qny)
-                search[qnx][qny] = 1
+                if(qnx>=0 and qnx<mx and qny>=0 and qny<my and search[qnx][qny]==0):
+                    queue.append([qnx, qny])
+                    search[qnx][qny] = 1
 
         # 전부 탐색을 성공했는지 아닌지
         check2 = True
@@ -73,8 +76,7 @@ def make_wall(list):
                     check2 = False
                     break
         if check2 == True:
-            list = temp
+            list = copy.deepcopy(temp)
         else:
             i-=1
-
     return list
